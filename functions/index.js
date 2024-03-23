4a0a3f7de793e30554ff67e48ef3561bac505a41
Bilal Motiwala <63motiwalas@gmail.com>
2024-03-23 14:08:52 +0800

    Start Creation of Token Frame

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+require("dotenv").config();
+const functions = require("firebase-functions");
+const firebaseAdmin = require("firebase-admin");
+const admin = firebaseAdmin.initializeApp();
+const { encodeFunctionData, parseEther } = require("viem");
+const { getFrameMessage, getFrameHtmlResponse } = require("@coinbase/onchainkit");
+const corsLib = require("cors");
+const cors = corsLib({ origin: "*" });
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createTokenFrame = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+  });
+});
